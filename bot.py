8a50d84bdee033f49b902ba2b54b02f401609295
Nika_Lu <stevetemor@gmail.com>
2025-02-02 04:46:24 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,11 +13,15 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: CallbackContext) -> None:
+    if not update.message:
+        return
     keyboard = [["Тест", "Ретроспектива", "Помощь"]]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
     await update.message.reply_text("Выберите действие:", reply_markup=reply_markup)
 
 async def handle_message(update: Update, context: CallbackContext) -> None:
+    if not update.message:
+        return
     user_id = update.message.from_user.id
     user_choice = update.message.text
     
@@ -27,11 +31,15 @@ async def handle_message(update: Update, context: CallbackContext) -> None:
     
     await update.message.reply_text("Функционал ещё в разработке")
 
+async def error_handler(update: object, context: CallbackContext) -> None:
+    logger.error(f"Exception while handling update {update}: {context.error}")
+
 def main() -> None:
     app = Application.builder().token(TOKEN).build()
     
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.add_error_handler(error_handler)
     
     app.run_polling()
 
