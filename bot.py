6f1fca7a293bada3cd03c3100f0f164a70d8071e
Nika_Lu <stevetemor@gmail.com>
2025-02-02 04:43:49 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,12 +12,12 @@ TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def start(update: Update, context: CallbackContext) -> None:
+async def start(update: Update, context: CallbackContext) -> None:
     keyboard = [["Тест", "Ретроспектива", "Помощь"]]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
-    update.message.reply_text("Выберите действие:", reply_markup=reply_markup)
+    await update.message.reply_text("Выберите действие:", reply_markup=reply_markup)
 
-def handle_message(update: Update, context: CallbackContext) -> None:
+async def handle_message(update: Update, context: CallbackContext) -> None:
     user_id = update.message.from_user.id
     user_choice = update.message.text
     
@@ -25,7 +25,7 @@ def handle_message(update: Update, context: CallbackContext) -> None:
     with open(f"data/test/{user_id}.txt", "w") as file:
         file.write(f"User ID: {user_id}\nВыбор: {user_choice}")
     
-    update.message.reply_text("Функционал ещё в разработке")
+    await update.message.reply_text("Функционал ещё в разработке")
 
 def main() -> None:
     app = Application.builder().token(TOKEN).build()
